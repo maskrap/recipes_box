df9eab871536be055554a61c31fc3e85de0d4e31
Leah Nelson and Sam Park <leah@>
2016-07-20 14:17:21 -0700

    Add callback to titlecase

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:tags)
+  validates(:name, :presence => true)
+  validates(:ingredients, :length => { :minimum => 2 })
+  before_save(:titleize_name)
+
+private
+
+  define_method(:titleize_name) do
+    self.name=(name().titleize())
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -4,7 +4,7 @@ describe(Tag) do
   describe('#recipes') do
     it('tells which recipes are in the tag') do
       test_tag = Tag.create({:category => 'exotic'})
-      test_recipe = Recipe.create({:ingredients => 'tiger bones', :instructions => 'in your mouth'})
+      test_recipe = Recipe.create({:name => "Soup", :ingredients => 'tiger bones', :instructions => 'in your mouth'})
       test_tag.recipes.push([test_recipe])
       expect(test_tag.recipes()).to(eq([test_recipe]))
     end
